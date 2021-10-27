a95388609abfb4e461e82ffcc7847146a232a803
Andres Robles <roblesandresm1@gmail.com>
2021-10-27 03:55:18 -0500

    rename divisors var for dividers

--- is_prime.py
+++ is_prime.py
@@ -1,6 +1,6 @@
 def is_prime(number: int) -> bool:
-    divisors = [n for n in range(1, number + 1) if number % n == 0]
-    return len(divisors) <= 2
+    dividers = [n for n in range(1, number + 1) if number % n == 0]
+    return len(dividers) <= 2
 
 def run():
     print(is_prime("4"))
